2306c3764ded25d6fa0fb70daa1c6beb04b180ce
Nathan Sit <nsit.earth@gmail.com>
2020-02-25 23:56:20 -0800

    Installation script now installs timer

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 # Timer installation. Requires sudo
 DESTDIR=/usr/bin/
+# Alarm location
+ALARMDIR=~/music/
 # Allows anyone to execute
 PERMISSIONS=755
 # Give permissions
 chmod $PERMISSIONS timer
-# Move to bin
+# Move timer to bin
 sudo cp timer $DESTDIR
-
+# Create directory for alarm
+mkdir -p $ALARMDIR
+# Copy alarm to new directory
+cp alarmCalm.mp3 $ALARMDIR
