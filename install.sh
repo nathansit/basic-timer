2550d9cf98e6837cb73a5ecb300f3b21f4b8f6b4
Nathan Sit <nsit.earth@gmail.com>
2020-03-14 22:15:29 -0700

    install script allows bin to be added to path automatically and sudo permissions are no longer required

--- install.sh
+++ install.sh
@@ -1,14 +1,26 @@
 # Timer installation. Requires sudo
-DESTDIR=/usr/bin/
+# UPDATE: no longer requires sudo, just does a local install
+# However, you will need to type the following command to add it to the path:
+DESTDIR=~/bin/
 # Alarm location
-ALARMDIR=~/music/
+ALARMDIR=~/Music/basic-timer-alarm/
 # Allows anyone to execute
 PERMISSIONS=755
 # Give permissions
 chmod $PERMISSIONS timer
-# Move timer to bin
-sudo cp timer $DESTDIR
-# Create directory for alarm
+# Create directories
+mkdir -p $DESTDIR
 mkdir -p $ALARMDIR
+
+# Adds bin to $PATH if it isn't already there
+	if [ -d "$DESTDIR" ] && [[ ":$PATH:" != *":$DESTDIR:"* ]]; then
+		echo export PATH=$PATH:$DESTDIR >> ~/.bashrc
+	fi
+source ~/.bashrc
+
+# Move timer to bin
+cp timer $DESTDIR
 # Copy alarm to new directory
 cp alarmCalm.mp3 $ALARMDIR
+
+echo "All done, simply run 'source ~/.bashrc' and give the command a try!"
